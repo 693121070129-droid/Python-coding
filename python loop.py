8fe116795a7334ed263147d48895ab4eada96354
RAIKA14 <693121070129@rmu.ac.th>
2026-09-11 15:18:02 +0700

    Add files via upload

---
+++ python loop.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number: "))
+for i in range(0, 13):
+    print(f"{i} x {num} = {i * num}")
+#for i in range(1, 13):
+#for j in range(1, 13):
+    #print(f"{i} x {j} = {i * j}")
+    
